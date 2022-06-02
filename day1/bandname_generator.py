623bce00b62fc544f063680c4821e24600e33a29
nsakgbmi6cia <theperson0004@gmail.com>
2022-06-02 14:00:40 +0200

    Day1 String Exercises Done

---
+++ day1/bandname_generator.py
@@ -0,0 +1,15 @@
+# the idea of the code is to get input concatnate and comment
+# I'm coding these projects as part of a 100-day challenge and I will code them both in python and Java
+
+
+#1. Create a greeting for your program.
+print("So you are starting a band? Let us get you a name for it!\n")
+#2. Ask the user for the city that they grew up in.
+city = input('Which city did you grow up in?\n')
+#3. Ask the user for the name of a pet.
+pet = input('And, what is your pet\'s name?\n')
+#4. Combine the name of their city and pet and show them their band name.
+print(f'Cool, You band name is: {city} {pet}')
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end

0404d115eabbd9e2fb26b581f9e65f29e2696225
nsakgbmi6cia <theperson0004@gmail.com>
2022-12-14 22:53:18 +0200

    this is my first stand alone project need a bit of help to function as intended

--- day1/bandname_generator.py
+++ day1/bandname_generator.py
@@ -9,7 +9,7 @@ city = input('Which city did you grow up in?\n')
 #3. Ask the user for the name of a pet.
 pet = input('And, what is your pet\'s name?\n')
 #4. Combine the name of their city and pet and show them their band name.
-print(f'Cool, You band name is: {city} {pet}')
+print(f"Cool, You band name is: {city} {pet}")
 
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://replit.com/@appbrewery/band-name-generator-end
